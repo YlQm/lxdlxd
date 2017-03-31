db5c481f321a0971cfcc6bf8c43b692a7dfd5c4b
zybchieffffff <chiefzhou1996@gmail.com>
2017-03-30 20:10:40 -0400

    Add files via upload

--- .idea/Sorcery/main.cpp
+++ .idea/Sorcery/main.cpp
@@ -6,47 +6,23 @@
 
 int main(int argc, char *argv[]){
         player player1, player2;
-//        deck deck1,deck2;
+
         string player1name, player2name;
         string initfilename;
-//       string deck1filename,deck2filename;
+
         string s;
         int turn = 0;
         int i = 0;
-//        for (int i = 0; i <= argc ; i++){
-                s = argv[i+1];
-//                if (s == "-init") {
-                        initfilename = argv[i+2];
-//                        i++;
- //               }
-//                       if (s == "-deck1"){
-//                        deck1filename = argc[i+2];
-//                        i++;
-//                }
-//                if (s == "-deck2"){
-//                        deck2filename = argc[i+2];
- //                       i++;
- //               }
-        }
-//        ifstream deck1file{deck1filename};
-//        string newcard;
-//        while(deck1filename >> newcard){
-//                deck1.addtodeck(newcard);
-//        }
-//        while(deck2filename >> newcard){
-//                deck2.addtodeck(newcard);
-//        }
+
+        s = argv[i+1];
+
+        initfilename = argv[i+2];
+
         ifstream initfile{initfilename};
         initfile >> player1name;
         initfile >> player2name;
         player1.setname(player1name);
         player2.setname(player2name);
-
-        
-        cout << player2.getname()<<endl;
-        while(initfile >> s){
-                cout << s << endl;
-        }
         try {
         while (1){
                 if (turn == 0) {
@@ -59,7 +35,7 @@ int main(int argc, char *argv[]){
                                 break;
                         }
                         if (s == "minus") {
-                                player1.changehealth("minus", 5);
+                                player1.changehealth("minus", 20);  //this is for testing the case when player's health lower than 0
                         }
 
                         if (s == "end") break;
@@ -75,7 +51,7 @@ int main(int argc, char *argv[]){
                                 break;
                         }
                         if (s == "minus") {
-                                player1.changehealth("minus", 5);
+                                player1.changehealth("minus", 20);
                         }
 
                         if (s == "end") break;
@@ -89,6 +65,4 @@ int main(int argc, char *argv[]){
         catch (string name){
                 cout << name << "lose" << endl;
         }
-
-
 }
